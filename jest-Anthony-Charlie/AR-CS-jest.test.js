62de91e9cd33ee4b0b24f4be0df66310aba172c4
Charlie19255 <charlie19255@gmail.com>
2022-11-01 16:48:56 -0700

    second part done

--- jest-Anthony-Charlie/AR-CS-jest.test.js
+++ jest-Anthony-Charlie/AR-CS-jest.test.js
@@ -54,14 +54,83 @@ const inBudget = (number) => {
 };
 // Write the test for a function that takes in two numbers and returns the smaller number.
 // Create the function that will make the test pass.
-// Write the test for a function that takes in one numbers and returns whether the number is odd.
+//input: num1/num2
+//output:num1
+describe("small", () => {
+  it("takes in two numbers and returns the smaller number", () => {
+    expect(small(num1, num2)).toEqual(num1);
+  });
+});
+// create a function called small. the function will take an argument of two numbers. we will use an if statement, where if the argument is less than num2 will return num1. if the argument is less than num1 will return num2.
+const num1 = 5
+const num2 = 10
+// expectedOutput : num1
+const small = (num1, num2) => {
+  if (num1 < num2) {
+    return num1;
+  } else if (num1 > num2) {
+    return num2;
+  }
+};
+
+// Write the test for a function that takes in one number and returns whether the number is odd.
+//input: num1
+//output: odd
+describe("odd", () => {
+  it("takes in one number and returns whether the number is odd", () => {
+    expect(odd(num1)).toEqual("odd");
+  });
+});
 // Create the function that will make the test pass.
+// create a function called odd. the function will take an argument of one number. we will use an if statement, where if the argument is odd will result in odd.
+const odd = (num1) => {
+  if (num1 % 2 !== 0) {
+    return "odd";
+  } 
+  };
 // Write the test for a function that takes in a fruit and returns "yellow" if the argument is banana, "red" if apple and "purple" if grape.
+//input:apple||grape||banana
+//output:red||purple||yellow
+describe("fruitColor", () => {
+  it("takes in a fruit and returns yellow if the argument is banana, red if apple and purple if grape", () => {
+    expect(fruitColor(fruit1)).toEqual("red");
+    expect(fruitColor(fruit2)).toEqual("purple");
+    expect(fruitColor(fruit3)).toEqual("yellow");
+  });
+});
+const fruit1 = "apple"
+const fruit2 = "grape"
+const fruit3 = "banana"
 // Create the function that will make the test pass.
+// create a function called fruitColor. the function will take an argument of one fruit. we will use an if statement, where if the argument is "apple" will result in "red".we will use an if statement, where if the argument is "grape" will result in "purple".we will use an if statement, where if the argument is "banana" will result in "yellow".
+const fruitColor = (fruit) => {
+  if (fruit === "apple") {
+    return "red"}
+    else if (fruit === "grape") {
+      return "purple"
+  } else if (fruit === "banana") {
+    return "yellow"}
+  };
 // Write the test for a function called rick that returns "Morty".
+//input:literally anything
+//output:Morty
+describe("rick", () => {
+  it("returns Morty", () => {
+    expect(rick()).toEqual("Morty");
+  });
+});
 // Create the function that will make the test pass.
+// create a function called rick. the function will take an argument of anything.It will "Morty".
+const rick = () => {return "Morty"}
 // Write the test for a function called greeter that takes a name as an argument and returns a greeting with that name to the screen.
+describe("greeter", () => {
+  it("takes a name as an argument and returns a greeting with that name to the screen", () => {
+    expect(greeter(name)).toEqual("How's it going,${name}");
+  });
+});
+const name = "Anthony"
 // Create the function that will make the test pass.
+const greeter = (name) => {return "How's it going,${name}"}
 // Write the test for a function called oddOrEven that takes a number as an argument and logs whether the number is odd or even.
 // Create the function that will make the test pass.
 // Write the test for a function called doubler that takes a number and returns the result of the number multiplied by 2.
