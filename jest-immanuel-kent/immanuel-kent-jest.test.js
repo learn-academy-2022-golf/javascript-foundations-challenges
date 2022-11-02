5f7e1963d82d2b602fde8144160375fe9e8b84ef
kentstone84 <kent.stone@gmail.com>
2022-11-02 11:36:04 -0500

    still working number 3

--- jest-immanuel-kent/immanuel-kent-jest.test.js
+++ jest-immanuel-kent/immanuel-kent-jest.test.js
@@ -1,5 +1,7 @@
 // Immanuel and Kent Golf 2002
 
+const { describe } = require("yargs")
+
 // Process: Write the test FIRST. Ensure the test fails correctly. Then write the code that will make the test pass.
 
 // // describe("greeter", () => {
@@ -33,9 +35,36 @@ const areUTired = (string) => {
 
 // Create the function that will make the test pass.
 // Write the test for a function that returns "relax" if you are stressed and "keep going" if you are not stressed.
+
+describe("areYouStressed", () => {
+  it("returns relax if you are stressed and keep going if you are not stressed", () => {
+    expect(areYouStressed("yes")).toEqual("relax")
+    expect(areYouStressed("no")).toEqual("keep going")
+  })
+})
 // Create the function that will make the test pass.
+const areYouStressed = (string) => {
+    if (string === "yes") {
+        return "relax"
+    } else if (string === "no") {
+        return "keep going"
+    }
+} 
+// result: Test Suites: 1 passed, 1 total
+// Tests: 2 passed, 2 total
+
+
 // Write the test for a function that returns "in budget" if a price is lower than $300.
-// Create the function that will make the test pass.
+describe("inBudget", () => {
+  it("returns in budget if a price is lower than 300", () => {
+    expect(inBudget(120)).toEqual("in budget")
+    expect(inBudget(100)).toEqual("in budget")
+  })
+})
+// Create the function that will make the test pass
+// 
+
+.
 // Write the test for a function that takes in two numbers and returns the smaller number.
 // Create the function that will make the test pass.
 // Write the test for a function that takes in one numbers and returns whether the number is odd.
