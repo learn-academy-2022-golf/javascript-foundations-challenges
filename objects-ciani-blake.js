ae0a14754dfc7df47cd74b32b46dd897acacc9ad
tabithatrubey <tabithatrubey@outlook.com>
2022-11-03 11:24:03 -0700

    Hospitality at its finest

--- objects-ciani-blake.js
+++ objects-ciani-blake.js
@@ -40,10 +40,10 @@ console.log(person.people())
     price: 24.99
   }
 
-  const totalWithTax = (object) => {
-    let prodMath = object.price * .07 
-    return Math.floor(object.price + prodMath)
-  }
+  // const totalWithTax = (object) => {
+  //   let prodMath = object.price * .07 
+  //   return Math.floor(object.price + prodMath)
+  // }
 
 //   Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
 
@@ -54,31 +54,48 @@ console.log(describeProduct(product))
 
 //   Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
 
-// const totalWithTax = (object) => {
-//   let prodMath = object.price * .07 
-//   return object.price + 
-// }
+const totalWithTax = (object) => {
+  let prodMath = (object.price * .07) + object.price 
+  return prodMath.toFixed(2)
+ }
 console.log(totalWithTax(product))
 //   Consider this variable:
   
-//   const lunch = {
-//     name: "PB and Banana",
-//     type: "sandwich",
-//     ingredients: ["bread", "peanut butter", "banana"]
-//   }
+  const lunch = {
+    name: "PB and Banana",
+    type: "sandwich",
+    ingredients: ["bread", "peanut butter", "banana"],
+    food: function () {
+      return `The ingredients for a ${this.name} ${this.type} are ${this.ingredients}.`
+    }
+  }
 //   Write the code that accesses the ingredients property.
+    console.log(lunch.ingredients)
+
 //   Write the code that access the third ingredient of the lunch object.
+  console.log(lunch.ingredients[2])
+
 //   Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+// const food = (object) => {
+//   return `The ingredients for a ${object.name} ${object.type} are ${object.ingredients}.`
+// }
+// console.log(food(lunch))
 //   Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+console.log(lunch.food())
+
 //   Consider this variable:
   
-//   const animals = [
-//     { name: "Waffles", type: "dog", age: 7 },
-//     { name: "Fluffy", type: "cat", age: 14 },
-//     { name: "Spelunky", type: "dog", age: 4 },
-//     { name: "Hank", type: "cat", age: 11 }
-//   ]
+
+
+  const animals = [
+    { name: "Waffles", type: "dog", age: 7 },
+    { name: "Fluffy", type: "cat", age: 14 },
+    { name: "Spelunky", type: "dog", age: 4 },
+    { name: "Hank", type: "cat", age: 11 }
+  ]
 //   Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+
 //   Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
 //   Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
 //   Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
