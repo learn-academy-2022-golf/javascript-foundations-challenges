123a7a2d833902e591a66d4fe7406f297c11d47b
cesarcuadro <cesarcuadro777@gmail.com>
2022-11-03 15:44:53 -0700

    completed challenges

--- classes-fb-cc.js
+++ classes-fb-cc.js
@@ -1,47 +1,49 @@
 // Challenges
 
 // Coffee marker: copy the given Coffee class into a text editor
-// class Coffee {
-//     constructor(type, cream, sugar) {
-//       this.type = type.toLowerCase()
-//       this.cream = cream
-//       this.sugar = sugar
-//     }
+class Coffee {
+    constructor(type, cream, sugar) {
+      this.type = type.toLowerCase()
+      this.cream = cream
+      this.sugar = sugar
+    }
   
-//     coffeeProfile() {
-//       return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
-//     }
+    coffeeProfile() {
+      return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
+    }
   
-//     creams() {
-//       if (this.cream > 1) {
-//         return `${this.cream} creams`
-//       } else {
-//         return `${this.cream} cream`
-//       }
-//     }
+    creams() {
+      if (this.cream > 1) {
+        return `${this.cream} creams`
+      } else {
+        return `${this.cream} cream`
+      }
+    }
   
-//     sugars() {
-//       if (this.sugar > 1) {
-//         return `${this.sugar} sugars`
-//       } else {
-//         return `${this.sugar} sugar`
-//       }
-//     }
-//   }
-// // Write the code that makes a black coffee object
-// let blackCoffee = new Coffee("black",0,0)
-// console.log(blackCoffee)
+    sugars() {
+      if (this.sugar > 1) {
+        return `${this.sugar} sugars`
+      } else {
+        return `${this.sugar} sugar`
+      }
+    }
+  }
 
-// // Write the code that outputs the black coffee's profile
-// console.log(blackCoffee.coffeeProfile())
+// Write the code that makes a black coffee object
+let blackCoffee = new Coffee("black",0,0)
+console.log(blackCoffee)
 
-// // Write the code that makes a coffee object with 1 cream and 2 sugars
-// let nonBlackCoffee = new Coffee("non-black",1,2)
-// console.log(nonBlackCoffee)
+// Write the code that outputs the black coffee's profile
+console.log(blackCoffee.coffeeProfile())
 
-// // Write the code that outputs the 1 cream and 2 sugars coffee profile
-// console.log(nonBlackCoffee.coffeeProfile())
+// Write the code that makes a coffee object with 1 cream and 2 sugars
+let nonBlackCoffee = new Coffee("non-black",1,2)
+console.log(nonBlackCoffee)
 
+// Write the code that outputs the 1 cream and 2 sugars coffee profile
+console.log(nonBlackCoffee.coffeeProfile())
+
+console.log("------------------------------------------------------------------------")
 // Latte Maker: create a class for Latte
 // Write a Latte class that takes a flavor, a milk type, and a number of shots
 class Latte {
@@ -71,10 +73,37 @@ let newLatte = new Latte("regular","none",1)
 // Log the regular, single shot latte's profile
 console.log(newLatte.latteProfile())
 
-
 // Write the code that makes a double shot, hazelnut latte with almond milk.
+let hazelnutLatte = new Latte("hazelnut", "almond", 2)
+
 // Log the double shot, hazelnut latte with almond milk's profile.
+console.log(hazelnutLatte.latteProfile())
+
+console.log("------------------------------------------------------")
 // Volume of a Cylinder: create a class for Cylinder
+class Cylinder {
+    constructor(radius, height) {
+      this.radius = radius
+      this.height = height
+      // this.pi = Math.pi
+    }
+  
+    cylinderEquation() {
+      return Math.PI * this.radius  * 2 * this.height
+    }
+  }
+
+
 // Write a class that calculates the volume of a Cylinder: v = πr2h (r is the radius and h is the height of the cylinder)
+let equation1 = new Cylinder(4, 2)
+console.log(equation1.cylinderEquation())
+
 // Write the code that rounds the volume of the cylinder to four decimal places
+console.log(equation1.cylinderEquation().toFixed(4))
+
 // Write the code that creates three unique cylinder objects
+const cylinder1 = new Cylinder()
+
+const cylinder2 = new Cylinder()
+
+const cylinder3 = new Cylinder()
